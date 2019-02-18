330a536e100b65ae4a8e136488d07a744f30696e
Henry Lay <henry88lay@gmail.com>
2019-02-18 16:47:05 +1100

    building out the game.js

--- js/Game.js
+++ js/Game.js
@@ -28,12 +28,70 @@
      }
 
     /**
-     * Selects random prashe from the phrases
+     * Selects random phrases from the phrases
      * @return {Object} Phrase object chosen to be used
      */
     getRandomPhrase() {
         return this.phrases[Math.floor(Math.floor(Math.random() * this.phrases.length))];
     }
 
-    
+    /**
+     * Begins game by selecting a random phrase and display to user.
+     */
+    startGame() {
+        document.getElementById('overlay').style.display = 'none';
+        this.activePhrase = this.getRandomPhrase();
+        this.activePhrase.addPhraseToDisplay();
+        this.resetGame(); // calls the reset game for a new game
+    }
+
+    /**
+     * Handles user interaction
+     */
+    handleInteraction (button) {
+
+        if(button.tagName === 'BUTTON') {
+
+            let letter = button.textContent; 
+            button.setAttribute('disabled',true);
+
+            let checkedLetter = this.activePhrase.checkedLetter(letter); //DRY Principle always!
+            
+            if(!checkedLetter) {
+                this.removeLife();
+                button.className = 'wrong'
+            } else {
+                button.className = 'chosen'
+            }
+
+            this.checkForWin();
+        }
+    }
+
+    /**
+     * Check for game win
+     * @return {boolean} true for win, false for loss
+     */
+
+    checkForWin(){
+        // check if all letters are revealed
+        let hidden = document.querySelectorAll('.hide');
+        if(hidden.length === 0) {
+            this.gameOver();
+        }
+    }
+
+    /**
+     * Remove life counter
+     */
+    removeLife(){
+        //increment this.missed by one
+        this.missed += 1;
+        // changes heart image
+        let hearts = document.querySelectorAll('.tries img');
+        hearts[this.missed -1].src = 'images/losingHeart.png';
+        if(this.missed === 5) {
+            this.gameOver();
+        }
+    }
  }
