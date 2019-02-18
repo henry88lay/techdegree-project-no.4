679281340a8868ebb8291cc5ad2da4d1be3f2f3e
Henry Lay <henry88lay@gmail.com>
2019-02-18 20:13:00 +1100

    completed the phrases js

--- js/Phrase.js
+++ js/Phrase.js
@@ -3,5 +3,44 @@
  * Phrase.js */
 
  class Phrase {
+     constructor(phrase) {
+         this.phrase = phrase.toLowerCase();
+     }
+
+     /**
+      * Display phrase on game
+      */
+     addPhraseToDisplay() {
+
+         for(let i = 0; i < this.phrase.length; i++) {
+            this.phrase[i].split('');
+            const li = document.createElement('li');
+            li.textContent = this.phrase[i];
+
+            if (this.phrase[i] === ' ') {
+                li.className = 'space';
+            } else {
+                li.className = `hide letter ${this.phrase[i]}`;
+            }
+
+            const phraseDiv = document.getElementById('phrase');
+            phraseDiv.querySelector('ul').appendChild(li);
+
+         }
+     }
      
+     /**
+      * Checking if passed letter in phrase
+      * @Param (string) letter - letter to check
+      */
+     checkLetter(letter) {
+         const phraseLetters = this.phrase.split('');
+         const matchLetters = phraseLetters.filter(letter => letter !== ' ');
+
+         if (matchLetters.includes(letter)) {
+             return true;
+         } else {
+             return false;
+         }
+     }
  }
