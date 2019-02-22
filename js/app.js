f5b88470a53c6c5abacb7ac30e06857d8bc1a2e9
Henry Lay <henry88lay@gmail.com>
2019-02-23 09:54:27 +1100

    fresh start rebuild the code base

--- js/app.js
+++ js/app.js
@@ -2,17 +2,3 @@
  * Project 4 - OOP Game App
  * app.js */
 
-const startGameButton = document.getElementById("btn__reset");
-const game = new Game();
-
-/* Start game by clicking, New game object is called */
-startGameButton.addEventListener('click', () => {
-    game.startGame();
-    game.resetGame();
-});
-
-
-document.getElementById('qwerty').addEventListener('click', (e) => {
-    game.handleInteraction(e.target);
-});
-
