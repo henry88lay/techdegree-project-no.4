fdd4bdc3e5bc4c415712167e3aea656a9c595b6b
Henry Lay <henry88lay@gmail.com>
2019-02-23 15:14:40 +1100

    building out the app and phrase js files

--- js/Phrase.js
+++ js/Phrase.js
@@ -23,5 +23,28 @@ class Phrase {
         }
     }
 
-    
+    /*This methods checks to see if the letter selected excists in the current game phrase.
+     *The Target that triggers the event is passed as an argument.
+     *The method will either return the letter contained in the prase or return null. */
+     checkLetter(letter) {
+         //letterFound is declared as false and will only become true if letter shows up in following loop.
+         let letterFound = false;
+         const letters = document.querySelectorAll('.letter');
+
+         for(let i=0; i < letters.length; i++) {
+             if (letters[i].textContent.toUpperCase() === letter.toUpperCase()) {
+                 letterFound = true;
+                 this.showMatchedLetter(letters[i]);
+             }
+         }
+            if(letterFound) {
+                return letter;
+            }
+            return null;
+     }
+
+     showMatchedLetter(letter) {
+         letter.className += " show"
+     }
+
 }
--- js/app.js
+++ js/app.js
@@ -2,4 +2,56 @@
  * Project 4 - OOP Game App
  * app.js */
 
-// Phrase class contains constructor method that accepts a string as a argument.
+// Global Variables
+const phrasesLiteral = [
+    "there is nothing permanent except change",
+    "you cannot shake hands with a clenched fist",
+    "Learning never exhuasts the mind",
+    "the only journey is the one within",
+    "independence is happiness"
+]
+const qwertyButtons = document.querySelector('#qwerty');
+const keys = document.querySelectorAll('.keys');
+const tries = docuement.querySelectorAll('li.tries img');
+const startButton = document.querySelector('#btn__reset');
+
+let game;
+let phrase;
+
+//Creates a new instance Game Object, and calls startGame and initialize methods to refresh.
+function startGame() {
+    game = new Game(phrasesLiteral);
+    game.startGame();
+    game.initialized();
+}
+
+// Start Game Overlay Hidden
+startButton.addEventListener('click', () => {
+    startGame();
+});
+
+//Event listener added to onscreen keyboard.
+qwertyButtons.addEventListener('click', (event) => {
+    if (event.target.tagName == 'BUTTON') {
+        let letter = event.target;
+        game.handleInteraction(letter);
+    }
+    game.checkForWin();
+});
+
+//Event listener triggered on keyboard.
+window.addEventListener('keypress', (e) => {
+    if(docuement.querySelector('#overlay').style.display === '') {
+        if (e.keyCode == '13') {
+            startGame();
+        }
+    }
+    game.activePhrase.checkLetter(event.key);
+    for(i=0; i<keys.length; i++) {
+        if(e.key === keys[i].textContent) {
+            let letter = keys[i];
+            game.handleInteraction(letter);
+        }
+    }
+    game.checkForWin();
+})
