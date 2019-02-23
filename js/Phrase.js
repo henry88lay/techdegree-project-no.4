94786d007c4b35682d79830a672bafb77d71fa23
Henry Lay <henry88lay@gmail.com>
2019-02-23 13:19:29 +1100

    building out the addPhraseToDisplay

--- js/Phrase.js
+++ js/Phrase.js
@@ -8,5 +8,20 @@ class Phrase {
         this.phrase = phrase;
     }
 
+    //This method lopps through the characters in the phrase and creates list items for each letter then appends them insight of the ul with the id #phrase
+    addPhraseToDisplay() {
+        const ulOfPhrases = document.querySelector('#phrase ul');
+        for(let i=0; i < this.phrase.length; i++) {
+        let liPhrase = document.createElement('li');
+        liPhrase.textContent = this.phrase[i];
+        if (liPhrase.textContent == ' ') {
+            liPhrase.className = 'space';
+        } else {
+            liPhrase.className = 'letter';
+        }
+        ulOfPhrases.appendChild(liPhrase);
+        }
+    }
+
     
 }
