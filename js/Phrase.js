6b9676af57618e3939c3801875ac18ab3d0b3973
Henry Lay <henry88lay@gmail.com>
2019-03-17 09:41:56 +1100

    Completed Step 1

--- js/Phrase.js
+++ js/Phrase.js
@@ -1,59 +1,7 @@
 /* Treehouse FSJS Techdegree
  * Project 4 - OOP Game App
  * Phrase.js */
-class Phrase {
-    constructor(phrase) {
-        //lowercase version of phrase set to Phrase object
-        this.phrase = phrase.toLowerCase();
-    }
 
-    /**
-    * Display phrase on game board
-    */
-    addPhraseToDisplay() {
-        //grab <ul> element
-        const ul = document.querySelector('ul');
-        //set variable to store string
-        let elem = '';
-        for (let i=0; i < this.phrase.length; i++) {
-                       
-            if (this.phrase[i] == ' ') {
-                elem += "<li class='space'> </li>";
-            } else {
-                elem += `<li class='hide letter ${this.phrase[i]}'>${this.phrase[i]}</li>`
-            }
-               
-         }
-
-         ul.innerHTML = elem;
-
-    }
-    /**
-    * Checks if passed letter is in phrase
-    * @param (string) letter - Letter to check
-    */
-    checkLetter(letter){
-        //return matching letter
-        return this.phrase.includes(letter);
-    }
-    /**
-    * Displays passed letter on screen after a match is found
-    * @param (string) letter - Letter to display
-    */    
-    showMatchedLetter(letter){
-        //set string for reference
-        const string = `.hide.letter.${letter}`;
-        //use string to match elements
-        const matchingLetters = document.querySelectorAll(string);
-        //for each matching element
-        for (let each of matchingLetters){
-            //remove the hide class
-            each.classList.remove('hide');
-            //add the show class
-            each.classList.add('show');
-        }
-
-    }
-
-
-}
+ class Phrase {
+     
+ };
--- js/app.js
+++ js/app.js
@@ -1,18 +1,9 @@
 /* Treehouse FSJS Techdegree
  * Project 4 - OOP Game App
  * app.js */
-let game = '';
-//add click event listener to reset button
-document.querySelector('#btn__reset').addEventListener('click' , function() {
-    //initializes a new Game object when clicked
-    game = new Game();
-    //calls the startGame() method on the new Game object
-    game.startGame();
-    game.handleKeyboard();
-});
-//add click event listeners to each onscreen key
-const keys = document.querySelectorAll('.key')
-keys.forEach(key => key.addEventListener('click', function(e) {
-    // call handleInteraction() on pressed key
-    game.handleInteraction(e.target);
-}));
+
+ let Game = '';
+
+ document.querySelector("btn__reset").addEventListener('click', function(){
+
+ });
