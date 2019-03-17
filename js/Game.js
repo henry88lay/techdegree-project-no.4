915c233253ca8b7a138c80f8c1284ed96ed3897f
Henry Lay <henry88lay@gmail.com>
2019-03-18 10:32:03 +1100

    added the phrase array

--- js/Game.js
+++ js/Game.js
@@ -20,5 +20,19 @@
         this.activePhrase = null;
         }
 
+    /**
+    * Creates phrases for use in game
+    * @return {array} An array of prhases that could be used in the game
+    */    
+    createPhrases() {
+        let phrasesArray = [
+            new Phrases('A happy soul in a happy body'),
+            new Phrases('A journey of a thousand miles must begin with a single step'),
+            new Phrases('A very interesting theory makes no sense at all'),
+            new Phrases('Feeling gratitude and not expressing it is like wrapping a present and not giving it'),
+            new Phrases('Humility is not thinking less of yourself, it’s thinking of yourself less'),
+        ]
+    };
+
 
  };
