2489dac32bfd9238831e2f7c0a5ed29839962b1f
Henry Lay <henry88lay@gmail.com>
2019-03-18 18:39:58 +1100

    added checkLetter and showMatchedLetter

--- js/Phrase.js
+++ js/Phrase.js
@@ -27,5 +27,32 @@
     }
     ul.innterHTML = element;
     };
+
+    /**
+    * Checks if passed letter is in phrase
+    * @param (string) letter - Letter to check
+    */
+    checkLetter(letter) {
+        //return matching letters
+        return this.phrase.includes(letter);
+    };
+
+    /**
+    * Displays passed letter on screen after a match is found
+    * @param (string) letter - Letter to display
+    */
+    showMatchedLetter(letter) {
+        // setting string for reference
+        const referenceString = `.hide.letter.${letter}`;
+        // matching string elements
+        const matchingLetters = document.querySelectorAll(referenceString);
+        // for each matching elements
+        for (let each of matchingLetters) {
+            //remove hide class
+            each.classList.remove('hide');
+            //add show class
+            each.classList.add('show');
+        }
+    };
  };
 
